867538cfdb0316a8ab92c5cb38b94ddf64c959a9
Arbnor Rexhepi <r.arbnor@outlook.com>
2022-11-07 15:06:43 +0100

    DOM manipulation on guess/number/score

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent); // Selecting a class, reading the textContent property of the element.
+
+document.querySelector('.message').textContent = '🎉Correct Number!'; // Selecting a class, writing the textContent property.
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 10; // on the Input element we use the 'value' to manipulate
