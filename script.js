c92d5e51fef1e8f8bfa14f429dbe4956a77326bf
Arbnor Rexhepi <r.arbnor@outlook.com>
2022-11-07 15:35:58 +0100

    Added functionality to the check button

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent); // Selecting a class, reading the textContent property of the element.
+// console.log(document.querySelector('.message').textContent); // Selecting a class, reading the textContent property of the element.
 
-document.querySelector('.message').textContent = '🎉Correct Number!'; // Selecting a class, writing the textContent property.
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.message').textContent = '🎉Correct Number!'; // Selecting a class, writing the textContent property.
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 10; // on the Input element we use the 'value' to manipulate
+// document.querySelector('.guess').value = 10; // on the Input element we use the 'value' to manipulate
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+}); //Event handler is the function inside the listener.
